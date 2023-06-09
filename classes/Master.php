c201c78c0809353699551c45f225317ec034fb1c
xpop134 <127761568+xpop134@users.noreply.github.com>
2023-06-09 22:10:04 +0800

    Add files via upload

---
+++ classes/Master.php
@@ -0,0 +1,216 @@
+<?php
+require_once('../config.php');
+Class Master extends DBConnection {
+	private $settings;
+	public function __construct(){
+		global $_settings;
+		$this->settings = $_settings;
+		parent::__construct();
+	}
+	public function __destruct(){
+		parent::__destruct();
+	}
+	function capture_err(){
+		if(!$this->conn->error)
+			return false;
+		else{
+			$resp['status'] = 'failed';
+			$resp['error'] = $this->conn->error;
+			return json_encode($resp);
+			exit;
+		}
+	}
+	function save_package(){
+		extract($_POST);
+		$data = "";
+		foreach($_POST as $k =>$v){
+			if(!in_array($k,array('id'))){
+				if(!is_numeric($v))
+					$v = $this->conn->real_escape_string($v);
+				if(!empty($data)) $data .=",";
+				$data .= " `{$k}`='{$v}' ";
+			}
+		}
+		if(empty($id)){
+			$sql = "INSERT INTO `package_list` set {$data} ";
+		}else{
+			$sql = "UPDATE `package_list` set {$data} where id = '{$id}' ";
+		}
+		$check = $this->conn->query("SELECT * FROM `package_list` where `name`='{$name}' ".($id > 0 ? " and id != '{$id}'" : ""))->num_rows;
+		if($check > 0){
+			$resp['status'] = 'failed';
+			$resp['msg'] = "Package Name Already Exists.";
+		}else{
+			$save = $this->conn->query($sql);
+			if($save){
+				$rid = !empty($id) ? $id : $this->conn->insert_id;
+				$resp['status'] = 'success';
+				if(empty($id))
+					$resp['msg'] = "Package details successfully added.";
+				else
+					$resp['msg'] = "Package details has been updated successfully.";
+			}else{
+				$resp['status'] = 'failed';
+				$resp['msg'] = "An error occured.";
+				$resp['err'] = $this->conn->error."[{$sql}]";
+			}
+		}
+		if($resp['status'] =='success')
+		$this->settings->set_flashdata('success',$resp['msg']);
+		return json_encode($resp);
+	}
+	function delete_package(){
+		extract($_POST);
+		$del = $this->conn->query("DELETE FROM `package_list` where id = '{$id}'");
+		if($del){
+			$resp['status'] = 'success';
+			$this->settings->set_flashdata('success',"Package has been deleted successfully.");
+		}else{
+			$resp['status'] = 'failed';
+			$resp['error'] = $this->conn->error;
+		}
+		return json_encode($resp);
+
+	}
+	function save_enrollment(){
+		if(empty($_POST['id'])){
+			$pref= date("Ym");
+			$code = sprintf("%'.04d",1);
+			while(true){
+				$check = $this->conn->query("SELECT * FROM `enrollee_list` where reg_no = '{$pref}{$code}'")->num_rows;
+				if($check > 0){
+					$code = sprintf("%'.04d",abs($code)+1);
+				}else{
+					break;
+				}
+			}
+			$_POST['reg_no'] = $pref.$code;
+		}
+		extract($_POST);
+		$data = "";
+		foreach($_POST as $k =>$v){
+			if(!in_array($k,array('id')) && !is_array($_POST[$k])){
+				if(!is_numeric($v))
+					$v = $this->conn->real_escape_string($v);
+				if(!empty($data)) $data .=",";
+				$data .= " `{$k}`='{$v}' ";
+			}
+		}
+		if(empty($id)){
+			$sql = "INSERT INTO `enrollee_list` set {$data} ";
+		}else{
+			$sql = "UPDATE `enrollee_list` set {$data} where id = '{$id}' ";
+		}
+		$save = $this->conn->query($sql);
+		if($save){
+			$aid = !empty($id) ? $id : $this->conn->insert_id;
+			$resp['status'] = 'success';
+			if(empty($id)){
+				$resp['reg_no'] = $reg_no;
+			}
+			if(empty($id))
+				$resp['msg'] = "Enrollment was successfully submitted";
+			else
+				$resp['msg'] = "Enrollee details was updated successfully.";
+		}else{
+			$resp['status'] = 'failed';
+			$resp['msg'] = "An error occured.";
+			$resp['err'] = $this->conn->error."[{$sql}]";
+		}
+		if($resp['status'] =='success')
+		$this->settings->set_flashdata('success',$resp['msg']);
+		return json_encode($resp);
+	}
+	function delete_enrollment(){
+		extract($_POST);
+		$del = $this->conn->query("DELETE FROM `enrollee_list` where id = '{$id}'");
+		if($del){
+			$resp['status'] = 'success';
+			$this->settings->set_flashdata('success',"Enrollee Records has deleted successfully.");
+		}else{
+			$resp['status'] = 'failed';
+			$resp['error'] = $this->conn->error;
+		}
+		return json_encode($resp);
+	}
+	function update_status(){
+		extract($_POST);
+		$update = $this->conn->query("UPDATE `enrollee_list` set `status` = '$status' where id = '{$id}'");
+		if($update){
+			$resp['status'] = 'success';
+			$this->settings->set_flashdata('success',"Enrollment Details has successfully updated.");
+		}else{
+			$resp['status'] = 'failed';
+			$resp['error'] = $this->conn->error;
+		}
+		return json_encode($resp);
+	}
+	function save_payment(){
+		extract($_POST);
+		$data = "";
+		foreach($_POST as $k =>$v){
+			if(!in_array($k,array('id'))){
+				if(!is_numeric($v))
+					$v = $this->conn->real_escape_string($v);
+				if(!empty($data)) $data .=",";
+				$data .= " `{$k}`='{$v}' ";
+			}
+		}
+		$sql = "INSERT INTO `payment_list` set {$data} ";
+		$save = $this->conn->query($sql);
+		if($save){
+			$rid = !empty($id) ? $id : $this->conn->insert_id;
+			$resp['status'] = 'success';
+			$resp['msg'] = "Payment details was added successfully.";
+		}else{
+			$resp['status'] = 'failed';
+			$resp['msg'] = "An error occured.";
+			$resp['err'] = $this->conn->error."[{$sql}]";
+		}
+		if($resp['status'] =='success')
+		$this->settings->set_flashdata('success',$resp['msg']);
+		return json_encode($resp);
+	}
+	function delete_payment(){
+		extract($_POST);
+		$del = $this->conn->query("DELETE FROM `payment_list` where id = '{$id}'");
+		if($del){
+			$resp['status'] = 'success';
+			$this->settings->set_flashdata('success',"Payment has deleted successfully.");
+		}else{
+			$resp['status'] = 'failed';
+			$resp['error'] = $this->conn->error;
+		}
+		return json_encode($resp);
+	}
+}
+
+$Master = new Master();
+$action = !isset($_GET['f']) ? 'none' : strtolower($_GET['f']);
+$sysset = new SystemSettings();
+switch ($action) {
+	case 'save_package':
+		echo $Master->save_package();
+	break;
+	case 'delete_package':
+		echo $Master->delete_package();
+	break;
+	case 'save_enrollment':
+		echo $Master->save_enrollment();
+	break;
+	case 'delete_enrollment':
+		echo $Master->delete_enrollment();
+	break;
+	case 'update_status':
+		echo $Master->update_status();
+	break;
+	case 'save_payment':
+		echo $Master->save_payment();
+	break;
+	case 'delete_payment':
+		echo $Master->delete_payment();
+	break;
+	default:
+		// echo $sysset->index();
+		break;
+}
